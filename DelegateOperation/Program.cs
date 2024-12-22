936d9f49827fe5136017f279423de3697dbd528d
azizallaev78@gmail.com <azizallaev78@gmail.com>
2024-12-22 16:34:10 +0500

    Добавьте файлы проекта.

---
+++ DelegateOperation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DelegateOperation
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Operation Operation1 = Minus;
+			Operation Operation2 = Plus;
+			Console.WriteLine(Operation1(2, 2));
+			Console.WriteLine(Operation1(2, 2));
+		}
+		public static int Plus(int a, int b) => a + b;
+		public static int Minus(int a, int b) => a - b;
+		public delegate int Operation(int x, int y);
+	}
+}
